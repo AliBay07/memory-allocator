30a8c7174248e38dfa70d7f1cbcff8c6f4bfcb33
Guegan Nicolas <guegann@im2ag-mandelbrot2.u-ga.fr>
2023-10-07 16:23:11 +0200

    ajout des tests unitaires pour mem_alloc

--- src/mem.c
+++ src/mem.c
@@ -62,91 +62,8 @@ void mem_init()
  **/
 void *mem_alloc(size_t size)
 {
+    printf("klc,dekl,c");
     mem_fit_function_t *get_correct_block = info->function_type;
-    size_t total_size_of_bb = size + sizeof(struct bb);
-    size_t min_size = sizeof(struct fb*);
-    struct fb *previous_block = (struct fb *)(*get_correct_block)((mem_free_block_t *)info->first, total_size_of_bb);
-    struct fb* current_block;
-    struct bb* new_bb;
-    
-    if(min_size>total_size_of_bb)
-    {
-        total_size_of_bb == min_size;
-    }
-    //1st case any bloc returned (first address returned)
-
-    //2nd case first block returned
-    if(previous_block == NULL)
-    {
-        printf("free size %zu\n",info->first->free_size);
-        if(info->first->free_size < size)
-        {
-            return NULL;
-        }
-        current_block = info->first;
-        //1st subcase only 1 free block in the list
-        if(current_block->next == NULL)
-        {
-            new_bb = (struct bb*)info->first;
-            new_bb->busy_size = size;
-            current_block =(struct fb*) ((char*)new_bb + sizeof(struct bb*)+ new_bb->busy_size); //si la zone est dans un fb
-            current_block->free_size -= size;
-            info->first = current_block;
-            printf("passe ici\n");
-        }
-        //2nd subcase more than 1 free block in the list, calculate remaining size to add a free block after busy block
-        if(current_block->next != NULL )
-        {
-            size_t remaining_size = current_block->free_size - total_size_of_bb;
-            if(remaining_size >= sizeof(struct fb*))
-            {
-                new_bb = (struct bb*)info->first;
-                new_bb->busy_size = size;
-                current_block =(struct fb*) ((char*)new_bb + sizeof(struct bb*)+ new_bb->busy_size); //si la zone est dans un fb
-                current_block->free_size = remaining_size;
-            }
-            else
-            {
-                new_bb = (struct bb*)info->first;
-                new_bb->busy_size = size+remaining_size;
-                info->first = current_block->next;   
-            }
-            
-        }
-        return (void *) (new_bb+1);
-    }
-    //3rd case a block is returned
-    if(previous_block != NULL)
-    {
-        printf("taille prev_block %zu\n",previous_block->free_size);
-        //4th case the last block is returned
-        if(previous_block->next == NULL)
-        {
-            printf("passe ici");
-            new_bb =(struct bb*) ((char*)previous_block+previous_block->free_size);
-            new_bb->busy_size = size;
-            previous_block->next = NULL;
-            return (void *) (new_bb+1);
-        }
-        current_block = previous_block->next;
-        size_t remaining_size = current_block->free_size - total_size_of_bb;
-        //1st calculate remaining size to add a free block after busy block
-        if(remaining_size >= sizeof(struct fb*))
-        {
-            new_bb = (struct bb*)current_block;
-            new_bb->busy_size = size;
-            current_block =(struct fb*) ((char*)new_bb + sizeof(struct bb*)+ new_bb->busy_size); //si la zone est dans un fb
-            current_block->free_size = remaining_size;
-        }
-        else
-        {
-            new_bb = (struct bb*)current_block;
-            new_bb->busy_size = size+remaining_size;
-            current_block = current_block->next;
-        }
-        return (void *) (new_bb+1);
-    }
-    
 
     size_t total_size = size + sizeof(struct bb);
 
@@ -297,66 +214,6 @@ size_t mem_get_size(void *zone)
  **/
 void mem_free(void *zone)
 {
-    struct fb* current_block_fb = info->first;
-    struct fb* previous_block_fb = NULL;
-    printf("adress of current_block allocated %p \n",info->first);
-    printf("adress of zone to free %p \n",zone);
-    //empecher free avant adresse info->first ou zone NULL
-    if (zone == NULL) {
-        printf("passe là\n");
-        return;
-    }
-    //1er cas la zone est deja libre (comprends le cas où le block suivant est un bb car on ne va pas écraser le bb suivant)
-    while(current_block_fb !=NULL && (char *)current_block_fb < (char*)zone ) //parcourt jusqu a trouver le bloc précédent
-    {
-        printf("taille previous_block %zu\n",previous_block_fb->free_size);
-        previous_block_fb = current_block_fb;
-        current_block_fb = current_block_fb->next;
-    }
-    //si la zone est en plein milieu d'un block
-    /*if((char*)zone>= (char*)previous_block_fb && (char*)zone<=previous_block_fb->free_size) //si la zone est dans un fb
-    {
-            return;
-    }*/
-
-    //2e cas la zone est occupee
-    struct bb* current_block_allocated = (struct bb*)(current_block_fb+current_block_fb->free_size)+sizeof(struct bb*);
-    printf("size of current_block allocated %zu \n",current_block_allocated->busy_size);
-
-    //parcours les bb jusqu'à trouver l'adresse de debut du bb
-    while((char*)current_block_allocated< (char*)zone)
-    {
-        current_block_allocated += sizeof(struct bb*) + current_block_allocated->busy_size;
-    }
-
-    //si l'adresse donnee n'est pas le début d'un bloc occupé 
-    if((char*)current_block_allocated != (char*)zone)
-    {
-        return;
-    }
-
-    struct fb* new_free_block = (struct fb*)current_block_allocated-sizeof(struct bb*);
-    
-    //si on libere le 1er bloc occupé
-    if(previous_block_fb == info->first->next) //on pointe le 1er bloc libre
-    {
-        info->first->next = new_free_block;
-    }
-
-    
-
-    /*new_free_block->next = (struct fb*)current_block_allocated;
-    new_free_block->free_size = sizeof(struct bb*) +current_block_allocated->busy_size;
-
-
-
-    
-     
-
-
-
-
-
 
     if (zone == NULL ||
         (zone - sizeof(struct bb)) < (mem_space_get_addr() + sizeof(struct info_alloc) + sizeof(struct fb)) ||
@@ -371,13 +228,10 @@ void mem_free(void *zone)
     struct fb *current_block = info->first;
     struct fb *prev_block = NULL;
 
-    while (current_block != NULL && (char*)current_block < (char*)new_fb)
     while (current_block != NULL && (char *)current_block < (char *)bb_ptr)
     {
         prev_block = current_block;
         current_block = current_block->next;
-        printf(" adresse de previous block free %p \n", prev_block);
-        printf(" adresse de current block free %p \n", current_block);
     }
 
     char *address_current_block;
@@ -444,18 +298,8 @@ void mem_free(void *zone)
     }
     else
     {
-        if (prev_block != NULL)
-        {
-            prev_block->next = new_fb;
-        }
-        else
-        {
-            info->first = new_fb;
-        }
-    }
-    {
         prev_block->next = new_fb;
-    }*/
+    }
 }
 
 //-------------------------------------------------------------
@@ -512,21 +356,17 @@ void mem_set_fit_handler(mem_fit_function_t *mff)
 mem_free_block_t *mem_first_fit(mem_free_block_t *first_free_block, size_t wanted_size)
 {
     struct fb *current_block = (struct fb *)first_free_block;
-    struct fb *previous_block = NULL;
-    int i =0;
     struct fb *previous_block = info->first;
 
     while (current_block != NULL)
     {
-        printf("i = %d\n",i);
-        printf("taille de current_block %zu \n",current_block->free_size);
         if (current_block->free_size >= wanted_size)
         {
             break;
         }
+
         previous_block = current_block;
         current_block = current_block->next;
-        i++;
     }
 
     return (mem_free_block_t *)previous_block;
--- tests/test_unitaire.c
+++ tests/test_unitaire.c
@@ -12,13 +12,49 @@
 //test de mem_alloc cas general, 1er block, dernier block
 
 
-static void * test_malloc(size_t size)
+static void  test_malloc_size0()
 {
-    if(size == 0)
-    {
-        return NULL;
-    }
-    
+    printf("first line size0");
+    void *ptr = mem_alloc(0);
+    assert(ptr == NULL);
+}
+
+static void  test_malloc(size_t size)
+{
+    printf("first line malloc");
+    void *ptr = mem_alloc(size);
+    assert(ptr != NULL);
+}
+
+static void  test_malloc_out_of_end_memory()
+{
+    printf("first line end memory");
+    size_t mem_size = mem_space_get_size();
+
+    size_t too_large_size = mem_size + 1;
+    void *ptr = mem_alloc(too_large_size);
+    assert(ptr == NULL);
+}
+
+static void test_malloc_out_of_start_memory()
+{
+    printf("first line start memory");
+    void *mem_start = mem_space_get_addr();
+    void *too_small_size = (void *)((char *)mem_start - 1);
+    void *ptr = mem_alloc((size_t)too_small_size);
+    assert(ptr == NULL);
+}
+
+
+
+int main(int argc, char *argv[]) {
+    printf("first line main");
+    test_malloc_size0();
+    test_malloc(16);
+    test_malloc_out_of_end_memory();
+    test_malloc_out_of_start_memory();
+
+    return 0;
 }
 
 
